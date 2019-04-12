452a349f04d48dd0e9c0200615a9b05fd36a41e8
sixon <397927931@qq.com>
2019-04-12 11:59:47 +0800

    Initial commit

---
+++ spring/src/main/java/com/sixon/testFor.java
@@ -0,0 +1,7 @@
+package com.sixon;
+
+/**
+ *
+ */
+public class testFor {
+}
---
+++ springmvc/src/main/java/com/sixon/testFor.java
@@ -0,0 +1,10 @@
+package com.sixon;
+
+/**
+ *
+ *
+ * controller中三种取参对比：
+ * @requestbody比较强大，能取类取json；@requestParam只能取get之后；@variable只能取url中
+ */
+public class testFor {
+}
